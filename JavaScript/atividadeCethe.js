edf9ecadd96bdc29760c3e959c076e072e22b185
tainandogomes-coder <tainando_gomes@estudante.sesisenai.org.br>
2026-03-24 21:40:32 -0300

    mias um

---
+++ JavaScript/atividadeCethe.js
@@ -0,0 +1,13 @@
+function aumentarImagem(){
+    let img = document.getElementById("imgPrincipal");
+
+    img.style.width = "500px"
+    img.style.height = "300px"
+}
+
+function diminuirImagem(){
+    let img = document.getElementById("imgPrincipal");
+
+    img.style.width = "200px"
+    img.style.height = "100px"
+}
